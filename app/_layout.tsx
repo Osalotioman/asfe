146a8d2da90baec29a456b18540ea1f350c9aae7
Osalotioman <osalotioman@gmail.com>
2024-12-14 23:00:22 +0100

    Reverting Back to Tabs.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,22 +30,10 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        {/*<Stack.Screen name="(tabs)" options={{ headerShown: false }} />*/}
-        <Stack.Screen name="index" />
-        <Stack.Screen name="about" />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
-
-/* /_sitemap takes you to the site map of the project. */
-
-/*
-import { Stack } from "expo-router";
-
-export default function RootLayout() {
-  return <Stack />;
-}
-*/
