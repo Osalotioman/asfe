cf4273f581d13122450ba908c23cd8ebdf9ede90
Osalotioman <osalotioman@gmail.com>
2024-12-14 23:20:25 +0100

    Working on homepage.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,13 +8,14 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      {/*headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
           source={require('@/assets/images/partial-react-logo.png')}
           style={styles.reactLogo}
         />
-      }>
+      }*/}>
+      {/*
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
@@ -50,6 +51,7 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
+      */}
     </ParallaxScrollView>
   );
 }
