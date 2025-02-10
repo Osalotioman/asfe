d3f2983bd798d50cace0dfa80d6042fda20da825
Ighalo Genesis Osasenaga <125825576+daves-hub@users.noreply.github.com>
2025-02-10 15:26:21 +0000

    fix: Bug fixes and warnings addressed

--- babel.config.js
+++ babel.config.js
@@ -4,10 +4,8 @@ module.exports = function (api) {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-      "module:metro-react-native-babel-preset",
     ],
     plugins : [
-      "react-native-reanimated/plugin",
       ['module:react-native-dotenv', {
         moduleName: '@env',
         path: '.env',
